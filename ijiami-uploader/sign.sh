1ae4323fc394588fff0fe6c2e8728888dda3ae5d
RexGene <rexgene@hotmail.com>
2016-10-17 10:01:10 +0800

    添加签名模块

---
+++ ijiami-uploader/sign.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ 1 != $# ]]; then
+  echo $0 "<input-file>"
+  exit 1
+fi
+
+inputFile=$1
+
+if [ ! -f "$inputFile" ]; then
+  echo "$inputFile not exist"
+  exit 1
+fi
+
+signedFile="${inputFile:0:-4}_signed.apk"
+
+jarsigner -digestalg SHA1 -sigalg MD5withRSA -keystore app.keystore -storepass 1881982050~\!@ -signedjar "$signedFile" "$1" app.keystore
+zipalign -v 4 "$signedFile" ${inputFile:0:-4}_final.apk
+
+rm -f $inputFile $signedFile
